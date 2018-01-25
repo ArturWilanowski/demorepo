6e0fbdebabf0f8dc0e1b56d66edbc297dd70fb89
Artur Wilanowski <arturwilanowski@gmail.com>
2018-01-25 15:14:39 +0100

    small change

--- src/main/java/pl/wilanowskiartur/demorepo/DemorepoApplication.java
+++ src/main/java/pl/wilanowskiartur/demorepo/DemorepoApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.convert.Jsr310Converters;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 
 @EntityScan(
-		basePackageClasses = {DemorepoApplication.class, Jsr310Converters.class}
+		basePackageClasses = {DemorepoApplication.class, Jsr310JpaConverters.class}
 )
 @SpringBootApplication
 public class DemorepoApplication {
